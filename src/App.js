87f708e5a1cf51d0e41847afd2c4f3622c798f35
Shubham Ayodhyavasi <shubhamgupta279@gmail.com>
2020-09-10 10:40:59 +0530

    material ui

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import { Route, Switch, withRouter } from "react-router-dom";
 import { connect } from 'react-redux'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
+      <Button className={classes.root}>
+        Hello World
+      </Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
