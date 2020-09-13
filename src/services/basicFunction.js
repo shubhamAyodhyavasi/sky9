c479019ae1144ccd04c97684211f150202ac38d7
AppleHD <kartik3rde@gmail.com>
2020-09-13 15:51:46 +0530

    daynamic video plyer

---
+++ src/services/basicFunction.js
@@ -0,0 +1,1 @@
+export const getQueryParams = () => window.location.search.replace('?', '').split('&').reduce((r,e) => (r[e.split('=')[0]] = decodeURIComponent(e.split('=')[1]), r), {});
--- src/view/pages/VideoDetails/VideoDetails.js
+++ src/view/pages/VideoDetails/VideoDetails.js
@@ -2,41 +2,37 @@ import React, { useEffect , useState} from 'react';
 import {getDaynamicPostData} from '../../../services/services'
 import Layout from '../../element/Layout'
 import ReactPlayer from 'react-player/lazy'
-import Slides from '../../element/Sliders/Slides/Slides'
 
 import "./VideoDetails.css"
-//import {IMG_URL} from '../../../constants'
-const IMG_URL="http://fantasymedia.in/sky9/uploads/"
-   
-
-const albumDetails={
-  title:"title",
-  id:"12",
-  details:"Each  has an image container, this is because you need the image to scale and move, we want the scaling to have a smooth transition. However if you add ...",
-  totalView:"10",
-  img:"https://akamaividz2.zee5.com/image/upload/w_1337,h_536,c_scale,f_auto,q_auto/resources/0-0-214663/cover/1170x658withlogo_756173185.jpg"
-}
-function VideoDetails() {
-    useEffect(() => {
-         const a=getDaynamicPostData('getVideoByalbumeId',{album_id:84})
-         console.log(a)
-      });
-      const items = new Array(10).fill(
-        {
-            img: 'https://akamaividz2.zee5.com/image/upload/w_1337,h_536,c_scale,f_auto,q_auto/resources/0-0-214663/cover/1170x658withlogo_756173185.jpg',
-            title: 'Camera',
-            width: '30%',
-        })
+const IMG_URL="http://fantasymedia.in/sky9/"
+ 
+function VideoDetails({album_id}) {
+     const [albumData ,setAlbumData]=useState([])
+     const [videoData ,setVideoData]=useState([])
+     
+         useEffect(async () => {
+            const response=await getDaynamicPostData('getVideoByalbumeId',{album_id:82})
+            setAlbumData(response?.records && response?.records.length && response?.records)
+            setVideoData(response?.records && response?.records.length && response?.records[0])
+           }, [album_id]);
+  
+     
+  
   return (
        <Layout >
-         <div className="video-player-wrapper">
+           {
+               albumData.length &&
+                <>
+                 <div className="video-player-wrapper">
                 <ReactPlayer
                 controls={true}
                 playing
                 width="100%"
-                playIcon={<img width="10%" src={`${IMG_URL}/play.png`} />}
-                url="https://thepaciellogroup.github.io/AT-browser-tests/video/ElephantsDream.mp4"
-                light="https://akamaividz2.zee5.com/image/upload/w_1337,h_536,c_scale,f_auto,q_auto/resources/0-6-1298/cover/1170x658withlog_1647246974.jpg"
+                playIcon={<img width="10%" src={`${IMG_URL}/uploads/play.png`} />}
+                
+                url={`${IMG_URL}/${videoData?.video_link}` }
+                //url="https://thepaciellogroup.github.io/AT-browser-tests/video/ElephantsDream.mp4"
+                light={`${IMG_URL}/${videoData?.image}` }
                 config={{ file: {
                     attributes: {
                         crossOrigin: 'true',
@@ -44,17 +40,17 @@ function VideoDetails() {
                     },
 
                     nodownload:true,
-                    tracks: [
-                    {kind: 'subtitles', src: 'https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt', srcLang: 'en', default: true},
-                    {kind: 'subtitles', src: 'https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt', srcLang: 'hi'}
-                    ]
+                    // tracks: [
+                    // {kind: 'subtitles', src: 'https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt', srcLang: 'en', default: true},
+                    // {kind: 'subtitles', src: 'https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt', srcLang: 'hi'}
+                    // ]
                 }}}
                 />
          </div>
          <div className="video-details-wrapper">
             <div className="video-details-title">
-               <h2>Movie title name</h2>
-               <p>Drama movie 2029 2h-30min</p>
+               <h2>{albumData[0].title}</h2>
+               <h3>{albumData[0].sub_cat_dt?.title}</h3>
             </div>
             <div className="video-details-info">
                <div className="video-details-info-single">
@@ -67,13 +63,29 @@ function VideoDetails() {
                </div>
             </div>
             <div className="video-details-subDetails">
-               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>{albumData[0].details}</p>
             </div>
             <div className="video-details-more-video">
                <h2>Trailers & More</h2>
-               <Slides items={items} allLink="/album" title="Title for slides" />
+               <div className="video-details-more-video-wrapper">
+                   {
+                       albumData?.map((data,index)=>
+                        <div key={index} className="video-details-more-video-cart">
+                            <a onClick={()=>{setVideoData(data)}}>
+                                <img src={`${IMG_URL}/${data?.image}` } />
+                                <h3>{data?.title}</h3>
+                            </a>
+                            </div>
+                       )
+                   }
+                   
+               </div>
+               
             </div>
          </div>
+               </>
+           }
+        
          
        </Layout>
   );
