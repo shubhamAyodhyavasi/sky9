32160d28dbee8e51b53f8503b42a951dd0c21218
Shubham Ayodhyavasi <shubhamgupta279@gmail.com>
2020-09-15 11:24:28 +0530

    cors proxy re-enable

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,7 @@
 export default {
     appName: 'sky9',
     IMG_URL: "http://fantasymedia.in/sky9/",
-    CORS_PROXY: "", //"https://cors-anywhere.herokuapp.com/",
+    CORS_PROXY: "https://cors-anywhere.herokuapp.com/",
     CATEGORY_ID_LIST: {
         home: 4,
     }
